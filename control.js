179a4908e6f7888699b4a1229fff83a87b263e6b
Rodrigo <rwesley2000@gmail.com>
2025-07-03 18:49:54 -0300

    adding tasklist

---
+++ control.js
@@ -0,0 +1,15 @@
+let counter = 0;
+let input = document.getElementById('inputTask');
+let btnAdd = document.getElementById('btn-add');
+let main = document.getElementById('taskList')
+
+function addTask() {
+    let inputValue = input.value;
+
+    if ((inputValue !=="") && (inputValue !== null) && (inputValue !== undefined)) {
+        
+        ++counter;
+
+        let newItem = ``
+    }
+}
